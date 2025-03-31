8886d3f20a70dd7f7fd630410ee6dd9577a1fac5
marshall-777 <marat.bostanbek@mail.ru>
2025-04-01 02:15:33 +0500

    Добавлен MainActivity как главный экран после входа

---
+++ app/src/main/java/com/example/storageapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.storageapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnLogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogout = findViewById(R.id.btn_logout);
+
+        btnLogout.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, Loginactivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
